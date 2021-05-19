941473db0234659463a76d2a77b204d23912eec5
Onkar <70532760+onkar2405@users.noreply.github.com>
2021-05-19 20:45:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+variable "access"{}
+variable "private"{}
+
+provider "aws"{
+    region="ap-south-1"
+    access_key = var.access
+    secret_key = var.private
+}
+
+resource "aws_instance" "app"{
+    ami="ami-0bcf5425cdc1d8a85" 
+    instance_type="t2.micro"
+}
